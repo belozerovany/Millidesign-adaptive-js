1b3668a41ff9cd312899493dbf1dccc3aff33037
belozerovany <belozerovany@gmail.com>
2017-01-16 12:23:03 +0200

    In this project i used library jQ when building slider and popup window. Also i use Masonry is a JavaScript grid for portfolio

---
+++ js/common.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function() {
+  var $container = $(".masonry-container");
+    $container.imagesLoaded(function () {
+        $container.masonry({
+            columnWidth: ".item",
+            itemSelector: ".item"
+        });
+    });  
+
+    $(".item").imagefill();   
+
+    jQuery('.item').hover(
+        function(){
+            $(this).find(".cover-portfolio").fadeIn();
+        },
+        function(){
+            $(this).find(".cover-portfolio").fadeOut();
+        }
+    );
+    $('.sl').slick({
+        prevArrow:'.slider .prev',
+        nextArrow:'.slider .next',
+    });
+
+    $(".header-form").click( function(){
+        $('.pop-outer').fadeIn('slow');
+    });
+    $(".close").click( function(){
+        $('.pop-outer').fadeOut('slow');
+    });
+});
+
